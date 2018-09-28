732f7a2c65899cb92a30eedbd9beeb6db0aecc04
stefansor <ssorobay00@stuy.edu>
2018-09-27 23:26:05 -0400

    0a

---
+++ ArrayDemo.java
@@ -0,0 +1,18 @@
+public class ArrayDemo{
+  public static void main(String[] args){
+    int[] booyea = {
+    100, 200, 300,
+    400, 500, 600,
+    700, 800, 900, 1000
+  };
+  printArray(booyea);
+  }
+  public static void printArray(int[]ary){
+    String s = "{";
+    for(int i = 0; i < ary.length; i = i + 1){
+      s = s + ary[i] + ",";
+    }
+    s = s + "}";
+    System.out.print(s);
+  }
+}
