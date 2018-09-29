46fbe97a121f1cacf9a9125342a11bc08f69344c
stefansor <ssorobay00@stuy.edu>
2018-09-29 12:35:18 -0400

    more

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -20,8 +20,45 @@ public class ArrayDemo{
   public static void printArray(int[][]ary){
     String rar = "{";
     for(int i = 0; i < ary.length; i = i + 1){
-      for(int j = 0; j < )
+      rar = rar + ary[i] + "}" + " " + "{";
+      for(int j = 0; j < ary[i].length;j = j + 1){
+        rar = rar + ary[i][j] + ",";
+      }
+      rar = rar + "}";
     }
-
+    System.out.print(rar);
   }
+
+  public static int countZeros2D(int[][] nums){
+    int zeros = 0;
+    for(int i = 0; i < nums.length; i = i + 1){
+      if(nums[i] == 0){
+        zeros = zeros + 1;
+      }
+      for(int j = 0; j < nums[i].length; j = j + 1){
+        if(nums[i][j] == 0){
+          zeros = zeros + 1;
+        }
+      }
+    }
+    return zeros;
+  }
+
+  public static void fill2D(int[][] vals){
+    for(int i = 0; i < vals.length; i = i + 1){
+      for(int j = 0; j < vals[i].length; j = j + 1){
+        vals[i][j] = 1;
+        if(i == j){
+          vals[i][j] = 3;
+        }
+      }
+    }
+    printArray(vals);
+  }
+
+  public static int[][] fill2DCopy(int[][] vals){
+    
+  }
+
+
 }
